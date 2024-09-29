d25fad9fd9a9116520464564ccab1dfb41f2a752
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-09-29 15:25:20 +0400

    daemata asakis bechdvis kodi ageGvelesiani brenchze

--- src/test/java/PrintPersonalInfo.java
+++ src/test/java/PrintPersonalInfo.java
@@ -1,8 +1,13 @@
 public class PrintPersonalInfo {
     public static void main(String[] args) {
         printname();
+        printage();
     }
     public static void printname() {
         System.out.println("Hello Tinatin");
+    }
+    public static void printage() {
+        int age = 25;
+        System.out.println(age);
     }
 }
