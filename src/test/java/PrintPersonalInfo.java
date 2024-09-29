5c6b5ecb6f16fafdd9a8c057834f508369f6bcc9
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-09-29 15:22:47 +0400

    daemata saxelis bechdvis kodi PrintPersonalInfo failshi

--- src/test/java/PrintPersonalInfo.java
+++ src/test/java/PrintPersonalInfo.java
@@ -1,2 +1,8 @@
 public class PrintPersonalInfo {
+    public static void main(String[] args) {
+        printname();
+    }
+    public static void printname() {
+        System.out.println("Hello Tinatin");
+    }
 }
